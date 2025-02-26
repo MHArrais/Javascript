35b172bba3e2ce59d1a58815f5fa5870ee906833
MHArrais <drake_i@hotmail.com>
2025-02-26 19:55:45 -0300

    feat: finalizando primitivos

---
+++ Tipos_primitivos/008 - Primitivo_Object/index.js
@@ -0,0 +1,16 @@
+/*
+Objeto: Tênis(propriedade) "cardarço", "palmilha"
+Objeto: Celular(Propriedade)
+Objeto: Drone(Propriedade)
+*/
+
+console.log(
+    {
+        roupas: {
+
+            tenis: ["Adidas", "Nike"],
+            camisa: ["Rednose", "Urban"],
+            calca: ["Jeans", "Pano"]
+        },
+    },
+)
