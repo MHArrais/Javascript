c8f08707db87367ab479c367b496112f42c5630d
MHArrais <drake_i@hotmail.com>
2025-02-26 21:03:14 -0300

    feat: Variaveis

---
+++ Variaveis/001 - Variaveis/index.js
@@ -0,0 +1,5 @@
+/*
+    var -> Ele é usado globalmente
+    let -> Ele é gerado dentro de um escopo
+    const -> Ele é gerado dentro de um escopo, porém podemos armazenar dados apenas 1x 
+*/
---
+++ Variaveis/003 - Escopo/index.js
@@ -0,0 +1,10 @@
+/*
+    Escopo:
+    Determina quais são os dados que podem
+    ser acessados em uma determinada parte do código.
+
+*/
+{
+    let nome = "Aline"
+    console.log(nome)
+}
