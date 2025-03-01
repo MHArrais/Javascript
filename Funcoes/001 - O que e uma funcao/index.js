c6f2625e5c3ff50d308901fc54fe60627db8483f
MHArrais <drake_i@hotmail.com>
2025-03-01 19:18:15 -0300

    feat: functions

---
+++ Funcoes/001 - O que e uma funcao/index.js
@@ -0,0 +1,25 @@
+/*
+    O que é uma função?
+    É um conjunto de instruções que executa uma tarefa ou calcula um valor.
+*/
+
+// 1 - Functions declaration
+function valor(){
+    const soma = 1+2;
+
+    return soma;
+}
+
+// 2 - Functions expression
+
+const expresso = function(){
+    return false;
+}
+
+// 3 - Arrow functions
+
+const flecha = () => {
+    const multi = 2*2;
+    return multi;
+};
+
